960d0ec64b38307e78a3a5257af7b6dfe3e1f504
luizy <luizy991212@gmail.com>
2023-03-04 20:53:50 +0900

    README add

--- src/PCRoom/PC.java
+++ src/PCRoom/PC.java
@@ -1,13 +1,41 @@
 package PCRoom;
 
+import java.util.Date;
+
 public class PC {
-    private PCAdministrator administrator = null;
     private int pcNumber;
 
+    private Date endTime;
 
-    public PC(PCAdministrator administrator, int pcNumber) {
-        this.administrator = administrator;
+
+    public PC(int pcNumber) {
         this.pcNumber = pcNumber;
+        endTime = new Date();
+    }
+
+    public void addTime(long min){
+        if (isTimeEnd()){
+            endTime.setTime(System.currentTimeMillis() + min*60*1000);
+        }
+        else {
+            endTime.setTime(endTime.getTime() + min*60*1000);
+        }
+        showEndTime();
+    }
+
+    public void showEndTime(){
+        System.out.println(this + " " + (endTime.getTime() - System.currentTimeMillis())/(1000*60) + "min left");
+    }
+
+    public void stop(){
+        if (!isTimeEnd())
+            endTime.setTime(System.currentTimeMillis());
+    }
+
+    public boolean isTimeEnd(){
+        if (endTime.getTime() <= System.currentTimeMillis())
+            return true;
+        return false;
     }
 
     @Override
